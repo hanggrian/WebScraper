6e5d3f25a89cee6714b352c82dba9120c37abe23
Hendra Anggrian <hendraanggrian@gmail.com>
2016-07-07 22:36:25 +0700

    v0.4.4

--- library/src/main/java/io/github/hendraanggrian/webreader/WebReader.java
+++ library/src/main/java/io/github/hendraanggrian/webreader/WebReader.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -38,8 +39,8 @@ public class WebReader extends WebView {
         if (!(context instanceof Activity))
             throw new RuntimeException("Please use Activity as first param of WebReader constructor, not plain Context.");
 
-        this.getSettings().setJavaScriptEnabled(true);
         this.callbacks = new ArrayList<>();
+        this.getSettings().setJavaScriptEnabled(true);
     }
 
     public WebReader addCallback(Callback callback) {
@@ -80,14 +81,20 @@ public class WebReader extends WebView {
         if (fetchLongUrl) {
             task = new ExpandUrlTask() {
                 @Override
+                protected void onPreExecute() {
+                    for (WebReader.Callback callback : callbacks)
+                        callback.onProgress(WebReader.this, 0);
+                }
+
+                @Override
                 protected void onPostExecute(Object o) {
                     if (o instanceof Exception) {
                         for (Callback callback : callbacks)
                             callback.onError(WebReader.this, (Exception) o);
 
                     } else if (o instanceof String) {
-                        setWebViewClient(getClient());
-                        addJavascriptInterface(getInterface(), WebReaderInterface.NAME);
+                        addJavascriptInterface(readerInterface, WebReaderInterface.NAME);
+                        setWebViewClient(readerClient);
                         WebReader.super.loadUrl(o.toString());
                     }
                 }
@@ -95,8 +102,8 @@ public class WebReader extends WebView {
             task.execute(url);
 
         } else {
-            setWebViewClient(getClient());
-            addJavascriptInterface(getInterface(), WebReaderInterface.NAME);
+            addJavascriptInterface(readerInterface, WebReaderInterface.NAME);
+            setWebViewClient(readerClient);
             WebReader.super.loadUrl(url);
         }
     }
@@ -104,49 +111,48 @@ public class WebReader extends WebView {
     public void stop() {
         if (task != null && !task.isCancelled())
             task.cancel(true);
-        setWebViewClient(null);
         removeJavascriptInterface(WebReaderInterface.NAME);
+        setWebViewClient(new WebViewClient());
+
         stopLoading();
+        clearHistory();
+        clearCache(true);
     }
 
     public void retry() {
         stop();
-        loadUrl(getUrl());
+        loadUrl(getOriginalUrl());
     }
 
-    private WebReaderClient getClient() {
-        return new WebReaderClient() {
-            @Override
-            public void onLoadResource(WebView view, String url) {
-                super.onLoadResource(view, url);
-                if (!finished)
+    private WebReaderClient readerClient = new WebReaderClient() {
+        @Override
+        public void onLoadResource(WebView view, String url) {
+            super.onLoadResource(view, url);
+            if (!finished)
+                for (Callback callback : callbacks)
+                    callback.onProgress(WebReader.this, view.getProgress());
+        }
+
+        @Override
+        public void onPageFinished(WebView view, final String url) {
+            super.onPageFinished(view, url);
+            view.loadUrl(WebReaderInterface.PROCESS_URL);
+        }
+    };
+
+    private WebReaderInterface readerInterface = new WebReaderInterface() {
+        @Override
+        @JavascriptInterface
+        public void processHTML(final String html) {
+            ((Activity) getContext()).runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
                     for (Callback callback : callbacks)
-                        callback.onProgress(WebReader.this, view.getProgress());
-            }
-
-            @Override
-            public void onPageFinished(WebView view, final String url) {
-                super.onPageFinished(view, url);
-                view.loadUrl(WebReaderInterface.PROCESS_URL);
-            }
-        };
-    }
-
-    private WebReaderInterface getInterface() {
-        return new WebReaderInterface() {
-            @Override
-            @JavascriptInterface
-            public void processHTML(final String html) {
-                ((Activity) getContext()).runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        for (Callback callback : callbacks)
-                            callback.onSuccess(WebReader.this, html);
-                    }
-                });
-            }
-        };
-    }
+                        callback.onSuccess(WebReader.this, html);
+                }
+            });
+        }
+    };
 
     public interface Callback {
         void onStarted(WebReader reader);
--- sample/src/main/java/io/github/hendraanggrian/webreadersample/MainActivity.java
+++ sample/src/main/java/io/github/hendraanggrian/webreadersample/MainActivity.java
@@ -2,12 +2,10 @@ package io.github.hendraanggrian.webreadersample;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 
@@ -24,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.editText) EditText editText;
     @BindView(R.id.webReader) WebReader webReader;
 
+    private MenuItem item_Status;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,11 +32,56 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         ExtraCollector.init(this);
+
+        webReader.addCallback(new WebReader.SimpleCallback() {
+            @Override
+            public void onProgress(WebReader reader, int progress) {
+                item_Status.setTitle(String.valueOf(progress) + "%");
+                item_Status.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        return false;
+                    }
+                });
+            }
+
+            @Override
+            public void onSuccess(WebReader reader, final String html) {
+                item_Status.setTitle("See HTML");
+                item_Status.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        Extras extras = new Extras()
+                                .putString(R.string.extra_title, webReader.getTitle())
+                                .putString(R.string.extra_subtitle, webReader.getUrl())
+                                .putString(R.string.extra_content, html);
+
+                        Intent intent = new Intent(MainActivity.this, ViewActivity.class);
+                        intent.putExtras(extras.toBundle());
+                        startActivity(intent);
+                        return true;
+                    }
+                });
+            }
+
+            @Override
+            public void onError(WebReader reader, Exception exc) {
+                exc.printStackTrace();
+                item_Status.setTitle("Error");
+                item_Status.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        return false;
+                    }
+                });
+            }
+        });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main, menu);
+        item_Status = menu.findItem(R.id.item_Status);
         return true;
     }
 
@@ -44,47 +89,13 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.item_Go:
-                webReader.removeAllCallbacks()
-                        .fetchLongUrl(true)
-                        .addCallback(new WebReader.SimpleCallback() {
-                            @Override
-                            public void onProgress(WebReader reader, int progress) {
-                                Snackbar.make(layout, "Loading... " + progress + "%", Snackbar.LENGTH_INDEFINITE).setAction("Stop", new View.OnClickListener() {
-                                    @Override
-                                    public void onClick(View view) {
-                                        webReader.stop();
-                                    }
-                                }).show();
-                            }
-
-                            @Override
-                            public void onSuccess(WebReader reader, final String html) {
-                                Snackbar.make(layout, "Completed! ", Snackbar.LENGTH_INDEFINITE).setAction("See HTML", new View.OnClickListener() {
-                                    @Override
-                                    public void onClick(View view) {
-                                        Extras extras = new Extras()
-                                                .putString(R.string.extra_title, webReader.getTitle())
-                                                .putString(R.string.extra_subtitle, webReader.getUrl())
-                                                .putString(R.string.extra_content, html);
-
-                                        Intent intent = new Intent(MainActivity.this, ViewActivity.class);
-                                        intent.putExtras(extras.toBundle());
-                                        startActivity(intent);
-                                    }
-                                }).show();
-                            }
-
-                            @Override
-                            public void onError(WebReader reader, Exception exc) {
-                                exc.printStackTrace();
-                                Snackbar.make(layout, "Error occured.", Snackbar.LENGTH_INDEFINITE).setAction("Retry", new View.OnClickListener() {
-                                    @Override
-                                    public void onClick(View view) {
-                                        webReader.retry();
-                                    }
-                                }).show();
-                            }
-                        }).loadUrl(editText.getText().toString());
+                webReader.loadUrl(editText.getText().toString());
+                return true;
+            case R.id.item_Stop:
+                webReader.stop();
+                return true;
+            case R.id.item_Retry:
+                webReader.retry();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
